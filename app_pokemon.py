f1ed9666e11195674cb808f1bf2adb0bb067a8e0
tonymiguelsantos44-star <tonymiguelsantos44@gmail.com>
2026-06-24 14:38:14 -0300

    Add files via upload

--- app_pokemon.py
+++ app_pokemon.py
@@ -39,3 +39,34 @@ with col2:
 
 with col3:
     st.metric('peso', f'{peso} KG')
+
+tipos, status, locais, habilidades = st.tabs(['tipos', 'status', 'locais', 'habilidade'])
+with tipos:
+    for i in dados_pokemon['types']:
+        st.markdown(f'- {i['type']['name']}')
+with status:
+
+    hp, ataque, defesa, ataque_esp, defesa_esp, velocidade = st.columns(6)
+    with hp:
+        st.metric('HP',  dados_pokemon['stats'][0]['base_stat'])
+    with ataque:
+        st.metric('Ataque',  dados_pokemon['stats'][1]['base_stat'])
+    with defesa:
+        st.metric('Defesa',  dados_pokemon['stats'][2]['base_stat'])
+    with ataque_esp:
+        st.metric('Ataque especial',  dados_pokemon['stats'][3]['base_stat'])
+    with defesa_esp:
+        st.metric('Defesa especial',  dados_pokemon['stats'][4]['base_stat'])
+    with velocidade:
+        st.metric('Velocidade',  dados_pokemon['stats'][5]['base_stat'])
+with locais:
+
+    locais =requests.get(dados_pokemon['location_area_encounters']).json()
+    for local in locais:
+        st.markdown(f'- {local['location_area']['name']}')
+
+with habilidades:
+    for habilidade in dados_pokemon['abilities']:
+        st.markdown(f'- {habilidade['ability']['name']}')
+
+
